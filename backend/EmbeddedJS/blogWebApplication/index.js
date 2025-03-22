8cb5b7007fd59c1dfe40fd5ab74a5331aece96d6
anton-buds <antonbuds7@outlook.com>
2025-03-22 21:35:33 +0800

    Creating a blog web application - Capstone Project

---
+++ backend/EmbeddedJS/blogWebApplication/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+// Logic
+
+
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
+app.get('/essay', (req, res) => {
+    res.render('essay.ejs');
+});
+app.get('/bio', (req, res) => {
+    res.render('bio.ejs');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
